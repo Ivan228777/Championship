86cafb7adf54df98c24ccc670e4ad3643c648642
Ivan228777 <55508174+Ivan228777@users.noreply.github.com>
2021-04-10 00:11:38 +0500

    Add files via upload

--- Задание/конечный вариант ( 1 и 2)/index2.js
+++ Задание/конечный вариант ( 1 и 2)/index2.js
@@ -1,15 +1,23 @@
-participants = [{"FName":"Василий","LName":"Петров","ID":"1","CompID":"1","Result":"52"},{"FName":"Иван","LName":"Иванов","ID":"2","CompID":"2","Result":"12"},{"FName":"Сидор","LName":"Иванов","ID":"3","CompID":"1","Result":"33"},{"FName":"Сидор","LName":"Сидоров","ID":"4","CompID":"1","Result":"12"},{"FName":"Емельян","LName":"Сидоров","ID":"5","CompID":"1","Result":"12"},{"FName":"Григорий","LName":"Светофоров","ID":"6","CompID":"2","Result":"3"},{"FName":"Степан","LName":"Степанов","ID":"7","CompID":"2","Result":"7"},{"FName":"Клим","LName":"Каторгин","ID":"8","CompID":"2","Result":"22"},{"FName":"Сергей","LName":"Сергеев","ID":"9","CompID":"1","Result":"90"},{"FName":"Степанида","LName":"Сидорова","ID":"10","CompID":"2","Result":"25"},{"FName":"Виктор","LName":"Клавиатуров","ID":"11","CompID":"3","Result":"11"},{"FName":"Петр","LName":"Фантиков","ID":"23","CompID":"4","Result":"17"},{"FName":"Елена","LName":"Троянова","ID":"17","CompID":"4","Result":"22"},{"FName":"Елена","LName":"Троянова","ID":"17","CompID":"2","Result":"6"},{"FName":"Елена","LName":"Троянова","ID":"17","CompID":"5","Result":"42"},{"FName":"Елена","LName":"Троянова","ID":"17","CompID":"6","Result":"2"},{"FName":"Виктор","LName":"Клавиатуров","ID":"11","CompID":"4","Result":"44"},{"FName":"Виктор","LName":"Клавиатуров","ID":"11","CompID":"5","Result":"14"},{"FName":"Виктор","LName":"Клавиатуров","ID":"11","CompID":"6","Result":"16"},{"FName":"Виктор","LName":"Клавиатуров","ID":"11","CompID":"1","Result":"1"},{"FName":"Василий","LName":"Петров","ID":"1","CompID":"2","Result":"11"},{"FName":"Сидор","LName":"Сидоров","ID":"3","CompID":"4","Result":"32"},{"FName":"Сидор","LName":"Сидоров","ID":"3","CompID":"5","Result":"66"},{"FName":"Сидор","LName":"Сидоров","ID":"3","CompID":"6","Result":"12"},{"FName":"Григорий","LName":"Светофоров","ID":"6","CompID":"6","Result":"21"},{"FName":"Григорий","LName":"Светофоров","ID":"6","CompID":"4","Result":"32"},{"FName":"Степан","LName":"Степанов","ID":"7","CompID":"2","Result":"21"},{"FName":"Степан","LName":"Степанов","ID":"7","CompID":"6","Result":"20"},{"FName":"Степан","LName":"Степанов","ID":"7","CompID":"5","Result":"71"},{"FName":"Василий","LName":"Петров","ID":"1","CompID":"4","Result":"33"},{"FName":"Василий","LName":"Петров","ID":"1","CompID":"6","Result":"31"},{"FName":"Сергей","LName":"Сергеев","ID":"9","CompID":"2","Result":"21"},{"FName":"Сергей","LName":"Сергеев","ID":"49","CompID":"1","Result":"90"},{"FName":"Сергей","LName":"Сергеев","ID":"49","CompID":"6","Result":"7"},{"FName":"Сергей","LName":"Сергеев","ID":"9","CompID":"6","Result":"12"}]
-competencies = [{"Name":"Разглядываение тяжестей","ID":"1"},{"Name":"HTML","ID":"2"},{"Name":"Песни","ID":"3"},{"Name":"JavaScript","ID":"5"},{"Name":".Net","ID":"4"},{"Name":"Java","ID":"6"}]
+async function getdata(){
+    const participantsResponse = await fetch("https://60704aa285c3f0001746fcaa.mockapi.io/participants")
+    const competenciesResponse = await fetch("https://60704aa285c3f0001746fcaa.mockapi.io/competencies")
+
+    const participantsResponseRead = await participantsResponse.json()
+    const competenciesResponseRead = await competenciesResponse.json() 
+    return{participants: participantsResponseRead, competencies:competenciesResponseRead}
+}
+
+function renderCompetencies(data) {
 const result = []
 
-for(let i = 0; i < participants.length; i++) {
+for(let i = 0; i < data.participants.length; i++) {
     const temp = {}
 
-    temp.participantName = `${participants[i].FName} ${participants[i].LName}`
+    temp.participantName = `${data.participants[i].FName} ${data.participants[i].LName}`
     temp.competencies = []
-    for (let j = 0; j < competencies.length; j++) {
-        if (competencies[j].ID === participants[i].CompID) {
-            const competence = `${competencies[j].Name}: ${participants[i].Result}`
+    for (let j = 0; j < data.competencies.length; j++) {
+        if (data.competencies[j].ID?.toString() === data.participants[i].CompID?.toString()) {
+            const competence = `${data.competencies[j].Name}: ${data.participants[i].Result}`
             temp.competencies.push(competence)
         }
     }
@@ -17,24 +25,7 @@ for(let i = 0; i < participants.length; i++) {
     result.push(temp)
 }
 
-console.log(result)
 
-
-
-
-function unique(arr) {
-    let result = [];
-  
-    for (i = 0 ; i < participants.length ; i++) {
-      if (!result.includes(i)) {
-        result.push(i);
-      }
-    }
-  
-    return result;
-  }
-  console.log( unique(participants) ); 
-function renderCompetencies(result) {
     for (let i = 0; i < result.length; i++) {
         const element = document.createElement('div')
 
@@ -56,4 +47,4 @@ function renderCompetencies(result) {
     }
 }
 
-renderCompetencies(result)
+getdata().then(data => renderCompetencies(data))
--- Задание/конечный вариант ( 1 и 2)/table-final.js
+++ Задание/конечный вариант ( 1 и 2)/table-final.js
@@ -20,31 +20,49 @@ async function getdata(){
           for (let j = 0; j < data.participants.length; j++) {
 
               if (data.participants[j].CompID?.toString() === data.competencies[i].ID?.toString()) {
-
-                  const participant = `${data.participants[j].FName} ${data.participants[j].LName}, ${data.participants[j].Result}`
+                  const participant = {name: `${data.participants[j].FName} ${data.participants[j].LName}`, result: data.participants[j].Result}
                   temp.participants.push(participant)
               }
           }
+
+          temp.participants = temp.participants.sort((a,b) => {
+              if (a.result > b.result) {
+                  return -1
+              } else if (a.result < b.result) {
+                  return 1
+              } else {
+                  return 0
+              }
+          })
+          console.log(temp.participants)
+          temp.participants = temp.participants.slice(0, 3)
           result.push(temp)
       }
+
+      console.log(result)
       
-      
-      
+      const element = document.createElement('table')
+
+      const firstRow = document.createElement("tr")
+      firstRow.innerHTML = `<td><b>Компетенция</b></td> <td><b>1</b></td> <td><b>2</b></td> <td><b>3</b></td>`
+
+      element.appendChild(firstRow)
+
+
       for (let i = 0; i < result.length; i++) {
-          const element = document.createElement('div')
-  
-          const competenceNameElement = document.createElement('p')
-          competenceNameElement.innerHTML = `<b>${result[i].competenceName}</b>`
-  
-          const participantsListElement = document.createElement('ol')
+          const participantsListElement = document.createElement("tr")
+
+          const compName = document.createElement('td')
+          compName.innerHTML = `<b>${result[i].competenceName}</b>`
+
+          participantsListElement.appendChild(compName)
           for (let j = 0; j < result[i].participants.length; j++) {
-              const participantsListItemElement = document.createElement('li')
-              participantsListItemElement.innerText = result[i].participants[j]
+              const participantsListItemElement = document.createElement('td')
+              participantsListItemElement.innerHTML = result[i].participants[j].name + ' ' + '<i>' + result[i].participants[j].result + '</i>'
   
               participantsListElement.appendChild(participantsListItemElement)
           }
   
-          element.appendChild(competenceNameElement)
           element.appendChild(participantsListElement)
   
           document.body.appendChild(element)
